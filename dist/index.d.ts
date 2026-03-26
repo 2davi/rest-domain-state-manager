9d05caf605db6c9b6a260383c6c202db9996e747
2davi <2davijean@gmail.com>
2026-03-26 09:23:02 +0900

    Remove build artifacts from tracking

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ApiHandler } from './src/network/api-handler.js';
-import { DomainState } from './src/domain/DomainState.js';
-import { DomainVO } from './src/domain/DomainVO.js';
-import { DomainPipeline } from './src/domain/DomainPipeline.js';
-import { DomainRenderer } from './src/plugins/domain-renderer/DomainRenderer.js';
-import { FormBinder } from './src/plugins/form-binder/FormBinder.js';
-import { closeDebugChannel } from './src/debug/debug-channel.js';
-export { ApiHandler, DomainState, DomainVO, DomainPipeline, DomainRenderer, FormBinder, closeDebugChannel };
